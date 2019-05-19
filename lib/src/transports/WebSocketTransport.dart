11c893507d483f856e549b95a6784c8a6c6ed139
湖北捷智云技术有限公司 <duanweiwei1982@gmail.com>
2019-05-19 11:37:58 +0800

    Update.

--- lib/src/transports/WebSocketTransport.dart
+++ lib/src/transports/WebSocketTransport.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 import 'package:events2/events2.dart';
 import '../logger.dart';
 
@@ -43,6 +44,7 @@ class WebSocketTransport extends EventEmitter {
       return message;
     } catch (error) {
       logger.failure('send() | error sending message: ' + error.toString());
+      throw error;
     }
   }
 
@@ -61,11 +63,50 @@ class WebSocketTransport extends EventEmitter {
     }
   }
 
+  Future<WebSocket> _connectForSelfSignedCert(String url) async {
+    try {
+      Random r = new Random();
+      String key = base64.encode(List<int>.generate(8, (_) => r.nextInt(255)));
+      SecurityContext securityContext = new SecurityContext();
+      HttpClient client = HttpClient(context: securityContext);
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) {
+        print('Allow self-signed certificate => $host:$port. ');
+        return true;
+      };
+      print(url);
+      HttpClientRequest request = await client.getUrl(
+          Uri.parse(url)); // form the correct url here
+      request.headers.add('Connection', 'Upgrade');
+      request.headers.add('Upgrade', 'websocket');
+      request.headers.add('Sec-WebSocket-Protocol', WS_SUBPROTOCOL);
+      request.headers.add(
+          'Sec-WebSocket-Version', '13'); // insert the correct version here
+      request.headers.add('Sec-WebSocket-Key', key.toLowerCase());
+
+      HttpClientResponse response = await request.close();
+      Socket socket = await response.detachSocket();
+      var webSocket = WebSocket.fromUpgradedSocket(
+        socket,
+        protocol: 'signaling',
+        serverSide: false,
+      );
+
+      return webSocket;
+    }catch(e){
+      throw e;
+    }
+  }
+
   void connect() async {
     try {
+
+      this._ws = await _connectForSelfSignedCert(this._url);
+      /*
       this._ws = await WebSocket.connect(this._url, headers: {
         'Sec-WebSocket-Protocol': WS_SUBPROTOCOL,
       });
+      */
       this._ws.listen((data) {
         logger.debug('Recivied data: ' + data);
         this.emit('message', decoder.convert(data));
--- test/proto_client_test.dart
+++ test/proto_client_test.dart
@@ -2,13 +2,19 @@ import 'package:protoo_client/protoo_client.dart';
 
 main() async {
 
-  Peer peer = new Peer('ws://127.0.0.1:4442/?peer-id=xxxxx');
+  Peer peer = new Peer('https://127.0.0.1:8443/ws?peer-id=xxxxx');
 
   peer.on('open', () {
 
     print('open');
 
     peer.send('login', {}).then((data) {
+      print('response: ' + data.toString());
+    }).catchError((error) {
+      print('response error: ' + error.toString());
+    });
+
+    peer.send('offer', {'sdp':'empty!'}).then((data) {
       print('response: ' + data.toString());
     }).catchError((error) {
       print('response error: ' + error.toString());
@@ -25,10 +31,11 @@ main() async {
 
   peer.on('request', (request, accept, reject) {
     print('request: ' + request.toString());
-    reject(404, 'Oh no~~~~~');
+    accept({ 'key1':"value1", 'key2':"value2"});
+    //reject(404, 'Oh no~~~~~');
   });
 
   await peer.connect();
 
-  peer.close();
+  //peer.close();
 }
