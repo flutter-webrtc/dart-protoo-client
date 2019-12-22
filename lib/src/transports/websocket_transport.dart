4d472cc8627080848667b1731431ddbcaef01238
CloudWebRTC <duanweiwei1982@gmail.com>
2019-12-22 22:17:08 +0800

    Add support for Flutter Web.

--- lib/src/transports/websocket_transport.dart
+++ lib/src/transports/websocket_transport.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:events2/events2.dart';
+import 'websocket_dart_impl.dart'
+    if (dart.library.js) 'websocket_web_impl.dart';
 import '../logger.dart';
 
 const APP_NAME = 'protoo-client';
@@ -17,7 +19,7 @@ const WS_SUBPROTOCOL = 'protoo';
 
 class WebSocketTransport extends EventEmitter {
   var _url;
-  var _ws;
+  WebSocketImpl _ws;
   var _closed;
   JsonDecoder decoder = new JsonDecoder();
   JsonEncoder encoder = new JsonEncoder();
@@ -34,14 +36,13 @@ class WebSocketTransport extends EventEmitter {
 
   get closed => this._closed;
 
-  Future<dynamic> send(message) async {
+  send(message) {
     if (this._closed) {
       throw 'transport closed';
     }
     try {
       logger.debug('send message: ' + encoder.convert(message));
-      this._ws.add(encoder.convert(message));
-      return message;
+      this._ws.send(encoder.convert(message));
     } catch (error) {
       logger.failure('send() | error sending message: ' + error.toString());
       throw error;
@@ -50,9 +51,7 @@ class WebSocketTransport extends EventEmitter {
 
   void close() {
     logger.debug('close()');
-
     if (this._closed) return;
-
     // Don't wait for the WebSocket 'close' event, do it now.
     this._closed = true;
     this.emit('close');
@@ -63,59 +62,30 @@ class WebSocketTransport extends EventEmitter {
     }
   }
 
-  Future<WebSocket> _connectForSelfSignedCert(String url) async {
-    try {
-      Random r = new Random();
-      String key = base64.encode(List<int>.generate(8, (_) => r.nextInt(255)));
-      SecurityContext securityContext = new SecurityContext();
-      HttpClient client = HttpClient(context: securityContext);
-      client.badCertificateCallback =
-          (X509Certificate cert, String host, int port) {
-        print('Allow self-signed certificate => $host:$port. ');
-        return true;
-      };
-      print(url);
-      HttpClientRequest request = await client.getUrl(
-          Uri.parse(url)); // form the correct url here
-      request.headers.add('Connection', 'Upgrade');
-      request.headers.add('Upgrade', 'websocket');
-      request.headers.add('Sec-WebSocket-Protocol', WS_SUBPROTOCOL);
-      request.headers.add(
-          'Sec-WebSocket-Version', '13'); // insert the correct version here
-      request.headers.add('Sec-WebSocket-Key', key.toLowerCase());
-
-      HttpClientResponse response = await request.close();
-      Socket socket = await response.detachSocket();
-      var webSocket = WebSocket.fromUpgradedSocket(
-        socket,
-        protocol: 'signaling',
-        serverSide: false,
-      );
-
-      return webSocket;
-    }catch(e){
-      throw e;
-    }
-  }
-
   void connect() async {
+    logger.debug('connecting to WebSocket ${this._url}');
     try {
+      this._ws = WebSocketImpl(this._url);
 
-      this._ws = await _connectForSelfSignedCert(this._url);
-      /*
-      this._ws = await WebSocket.connect(this._url, headers: {
-        'Sec-WebSocket-Protocol': WS_SUBPROTOCOL,
-      });
-      */
-      this._ws.listen((data) {
+      this._ws.onOpen = () {
+        _closed = false;
+        this.emit('open');
+      };
+
+      this._ws.onMessage = (data) {
         logger.debug('Recivied data: ' + data);
         this.emit('message', decoder.convert(data));
-      }, onDone: () {
+      };
+
+      this._ws.onClose = (closeCode, closeReason) {
         logger.debug('Closed by server!');
+        this._closed = true;
         this.emit('close');
-      });
-      this.emit('open');
+      };
+
+      await this._ws.connect(headers: {});
     } catch (e) {
+      this._closed = true;
       this.emit('error', e.toString());
     }
   }
